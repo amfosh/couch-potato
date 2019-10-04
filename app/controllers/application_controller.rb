04ed0d50938afd54f4b7e6b0d2a5d33fbbd99063
alicia <afasciocco@gmail.com>
2019-10-04 07:41:16 -0400

    Signup Button working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,29 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "secret"
+    enable :sessions
   end
 
   get "/" do
     erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+  get '/signup' do
+    if !logged_in?
+        erb :"users/new"
+    else
+        redirect "/shows"
+    end
   end
 end
