5c5b25a2cab1f788f7bb92299bb7cc7257d4848c
alicia <afasciocco@gmail.com>
2019-10-04 07:46:00 -0400

    Login page working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,22 @@ class ApplicationController < Sinatra::Base
         redirect "/shows"
     end
   end
+
+  get '/login' do
+    if logged_in?
+        redirect "/tweets"
+    else 
+      erb :"/users/login"
+    end
+end
+  
+post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/shows'
+    else
+        redirect '/signup'
+    end
+  end
 end
