f8d9b014ef39b875a1658599b454b43ec5ad50fe
alicia <afasciocco@gmail.com>
2019-10-17 20:14:39 -0400

    added presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :email, uniqueness: true
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
     has_many :shows
     
     def slug
