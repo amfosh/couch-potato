db6dd3828c0479537698b95d491941f4a3fb512b
alicia <afasciocco@gmail.com>
2020-05-14 09:09:53 -0400

    schema

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,6 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 Dotenv.load if ENV['SINATRA_ENV'] == "development"
 
 set :database_file, "./database.yml" 
+
 require './app/controllers/application_controller'
 require_all 'app'
