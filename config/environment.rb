19ca047aecb30c80e4e81b6c08b3ae00876976b8
alicia <afasciocco@gmail.com>
2020-05-14 08:09:21 -0400

    bundle

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-set :database_file, "./database.yml"
-
+set :database_file, "./database.yml" 
 require './app/controllers/application_controller'
 require_all 'app'
